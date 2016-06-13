b9fd0436116da3ebcce5618df7e0ca949e8036e5
Will <willj@users.noreply.github.com>
2016-06-13 15:53:58 +0100

    Add CSV response type for asp.net mvc

---
+++ CSVExtensions/CSVExtensions/CsvFileResult.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MBW
+{
+    public class CsvFileResult<T> : FileResult
+    {
+        private IEnumerable<string> data;
+
+        public CsvFileResult(IEnumerable<T> data, string fileName)
+            : base("text/csv")
+        {
+            this.data = data.ToCsv();
+            this.FileDownloadName = fileName;
+        }
+
+        protected override void WriteFile(HttpResponseBase response)
+        {
+            foreach (var item in data)
+            {
+                response.Write(item);
+            }
+        }
+    }
+}
